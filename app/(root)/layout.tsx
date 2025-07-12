dc307c7c397360e7c20b1c98b98a8837261c6e31
Eddie Ibarra <eddieibarra43@gmail.com>
2025-07-12 11:46:41 -0500

    interview card

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,19 @@
 import React, {ReactNode} from 'react'
+import Link from "next/link";
+import Image from "next/image";
 
 const RootLayout = ({children} : {children: ReactNode}) => {
     return (
-        <div>{children}</div>
+       <div className="root-layout">
+           <nav>
+               <Link href="/" className="flex items-center gap-2">
+                   <Image src="/logo.svg" alt="logo" height={32} width={38} />
+                   <h2 className="text-primary-100">PrepWise</h2>
+               </Link>
+               {children}
+           </nav>
+
+       </div>
     )
 }
 export default RootLayout
